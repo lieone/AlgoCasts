eff789bd578fde83a9279023798827cc04d8b9d8
gorodetski.konstantin <kgorodetsky@yandex.ru>
2020-01-29 12:30:05 +0300

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let sign = Math.sign(n);
+    let abs = Math.abs(n);
+    return sign * parseInt(n.toString().split('').reverse().join(''));
+}
 
 module.exports = reverseInt;
