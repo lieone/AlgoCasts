e43d0c60e7be186505df85b152d620a5b17d9987
gorodetski.konstantin <kgorodetsky@yandex.ru>
2020-01-31 09:36:39 +0300

    max chars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,12 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars = {};
+    str.split('').forEach(char => chars[char] = chars[char] + 1 || 1);
+    return Object.entries(chars).reduce((max, entry) => {
+        return max[1] > entry[1] ? max : entry;
+    }, ['', 0])[0];
+}
 
 module.exports = maxChar;
