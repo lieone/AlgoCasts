c18618f23fac44b556180b2d2c22a857def94a9d
gorodetski.konstantin <kgorodetsky@yandex.ru>
2020-02-03 16:10:32 +0300

    Chunk alternate with slise

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,18 +9,28 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    let result = [];
-    let subArray;
-    let step = 0;
-    for (let item of array) {
-        if (step === 0) {
-            subArray = [];
-            result.push(subArray);
-        }
-        subArray.push(item);
-        step = step === size - 1 ? 0 : step + 1;
+    const result = [];
+    let chunkPointer = 0;
+    while (chunkPointer < array.length) {
+        result.push(array.slice(chunkPointer, chunkPointer + size));
+        chunkPointer += size;
     }
     return result;
 }
+
+// function chunk(array, size) {
+//     let result = [];
+//     let subArray;
+//     let step = 0;
+//     for (let item of array) {
+//         if (step === 0) {
+//             subArray = [];
+//             result.push(subArray);
+//         }
+//         subArray.push(item);
+//         step = step === size - 1 ? 0 : step + 1;
+//     }
+//     return result;
+// }
 
 module.exports = chunk;
