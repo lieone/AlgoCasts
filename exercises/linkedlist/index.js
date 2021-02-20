e2ec26b1d6af6bf73aa5726e441a06486a6be8a5
kstgrd <kstgrd@gmail.com>
2021-02-20 17:31:36 +0300

    linked list finished

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -20,10 +20,107 @@ class LinkedList {
 
   size() {
     let size = 0;
-    for (let node = this.head; node != null; node = node.next) {
+    for (let node = this.head; !!node; node = node.next) {
       size += 1;
     }
     return size;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    let last = this.head;
+    while (last && last.next) {
+      last = last.next;
+    }
+    return last;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    this.head = this.head && this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let node = this.head;
+    while (node.next.next) {
+      node = node.next;
+    }
+    node.next = null;
+  }
+
+  insertLast(data) {
+    if (!this.head) {
+      this.insertFirst(data);
+      return;
+    }
+    this.getLast().next = new Node(data);
+  }
+
+  getAt(index) {
+    let node = this.head;
+    let i = 0;
+    while (node && i < index) {
+      i += 1;
+      node = node.next;
+    }
+    return node;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+    const prev = this.getAt(index - 1);
+    if (prev.next) {
+      prev.next = prev.next.next;
+    }
+  }
+
+  insertAt(data, index) {
+    if (index === 0) {
+      this.insertFirst(data);
+      return;
+    }
+    const prev = this.getAt(index - 1);
+    if (prev) {
+      prev.next = new Node(data, prev.next);
+    } else {
+      this.insertLast(data);
+    }
+  }
+
+  forEach(consumer) {
+    let node = this.head;
+    let index = 0;
+    while (node) {
+      consumer.apply(this, [node, index++]);
+      node = node.next;
+    }
+  }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
   }
 }
 
